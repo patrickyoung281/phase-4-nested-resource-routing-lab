90eb001a30269eb3da6b491f12d8e384619ecdd1
Patrick Young <patrickyoung281@gmail.com>
2023-01-18 15:41:11 -0500

    commit tree ef04e0093398abe41821cd2d2989f444761945c0

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,30 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all 
+    end
     render json: items, include: :user
+  end
+
+  def show
+  item = Item.find(params[:id])
+  render json: item, include: :user
+  end
+
+def create 
+  item = Item.create(name: params[:name], price: params[:price], description: params[:description], user_id: params[:user_id])
+  render json: item, status: :created
+end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "item not found" }, status: :not_found
   end
 
 end
